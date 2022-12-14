1a36823e2e3195bf12ca46dc2745a0ca7db6f689
Bliznyuk <bliznyuk.ml@gmail.com>
2022-12-14 02:28:22 +0200

    Task completed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,43 @@
 package org.example;
 
+import java.io.*;
+
 /**
- * Hello world!
- *
+ * Створити 10 обчислювальних потоків (Thread), що повинні працювати одночасно.
+ * Кожен потік буде зчитувати порцію даних з потоку даних InputStream, і записувати в OutputStream.
+ * Задача полягає в тому, щоб усі потоки працювали по черзі.
+ * Для цього застосовуйте синхронізацію або через synchronized або за допомогою ReentrantLock
+ * <p>
+ * Продемонструвати роботу потоків на прикладі копіювання великого текстового файлу, наприклад, alica.txt.
+ * <p>
+ * Вихідний файл повинен мати той самий зміст, що і оригінальний.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+public class App {
+
+    public static void main(String[] args) throws IOException, FileNotFoundException {
+
+        try (
+                InputStream in = new FileInputStream("alice.txt");
+                OutputStream out = new FileOutputStream("alice_copy.txt");
+        ) {
+            ReWriter[] threads = new ReWriter[10];
+            for (int i = 0; i < 10; i++) {
+                threads[i] = new ReWriter("ReWriter #" + i, in, out);
+            }
+
+            for (ReWriter th : threads) {
+                th.start();
+            }
+
+            for (ReWriter th : threads) {
+                try {
+                    th.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
--- src/main/java/org/example/ReWriter.java
+++ src/main/java/org/example/ReWriter.java
@@ -2,30 +2,41 @@ package org.example;
 
 import java.io.*;
 
-public class MyReader {
+public class ReWriter extends Thread{
 
-    static String str = "";
-    static char[] buffer;
-    static int count;
+    private final InputStream in;
+    private final OutputStream out;
 
-    public static void reWriter() {
-        try (Reader rdr = new FileReader("alice.txt");
-             Writer wr = new FileWriter("alice_new.txt")) {
-            char[] buffer = new char[1024];
-            while ((count = rdr.read(buffer)) > 0) {
-//                str = new String(buffer, 0, count);
-//                System.out.print(str);
-                wr.write(buffer, 0, count);
-            }
-            System.out.print(str);
-        } catch (
-                FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (
-                IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static final int VALUE = 1024;
+
+    public ReWriter(String name, InputStream in, OutputStream out) {
+        super(name);
+        this.in = in;
+        this.out = out;
     }
 
-
+    @Override
+    public void run() {
+        while (true){
+            byte [] buffer = new byte[VALUE];
+            synchronized (in) {
+                try {
+                    int leng = in.read(buffer);
+                    if (leng > 0) {
+                        out.write(buffer, 0, leng);
+                        System.out.println("Thread " + getName() + " writes " + leng + " bytes");
+                    } else {
+                        break;
+                    }
+                } catch (IOException e) {
+                    break;
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
